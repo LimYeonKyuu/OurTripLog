b32018f386b8c74db995c9db37baa7ce40033ad8
LimYeonKyuu <lim000606@naver.com>
2025-07-08 15:55:56 +0900

    init: initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello, Vite + React + MUI!</h1>
+                <Button variant="contained">Click me</Button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
