ee80406df232a326331942864d5ed543f64fa47b
LimYeonKyuu <lim000606@naver.com>
2025-07-09 14:43:22 +0900

    feat: 로그 기능 추가

--- backend/src/main/java/org/example/backend/user/service/UserService.java
+++ backend/src/main/java/org/example/backend/user/service/UserService.java
@@ -2,6 +2,7 @@ package org.example.backend.user.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.backend.base.exception.AlreadyExistException;
 import org.example.backend.user.dto.UserDto;
 import org.example.backend.user.entity.User;
@@ -11,11 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
 
     public void join(UserDto dto) {
         if (userRepository.existsById(dto.getId())) {
+            log.warn("Attempt to join with existing ID: {}", dto.getId());
             throw new AlreadyExistException("이미 존재하는 아이디입니다.");
         }
         userRepository.save(User.join(dto));
